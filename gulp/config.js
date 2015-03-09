84f38f6120181998d25626115247b5b18674e629
zhangaz1 <zhangaz1@hotmail.com>
2015-03-09 13:36:41 +0800

    增加js压缩任务

--- gulp/config.js
+++ gulp/config.js
@@ -3,58 +3,58 @@
 var pathHelper = require('path-helper')(__dirname);
 
 var config = {
-    homePage: 'http://localhost:3000/',
+	homePage: 'http://localhost:3000/',
 
-    appStyleFile: pathHelper.resolve('./../src/client/app.css'),
-    clientAppJsFile: pathHelper.resolve('./../src/client/app.js'),
-    clientBuildDir: pathHelper.resolve('./../src/client/build'),
-    clientComponentsDir: pathHelper.resolve('./../src/client/components'),
+	appStyleFile: pathHelper.resolve('./../src/client/app.css'),
+	clientAppJsFile: pathHelper.resolve('./../src/client/app.js'),
+	clientBuildDir: pathHelper.resolve('./../src/client/build'),
+	clientComponentsDir: pathHelper.resolve('./../src/client/components'),
 
-    clientFiles: [
-        pathHelper.resolve('./../src/client/index.html'),
-        pathHelper.resolve('./../src/client/loadResource.js'),
-        pathHelper.resolve('./../src/client/favicon.ico'),
-        pathHelper.resolve('./../src/client/bower.json'),
-        pathHelper.resolve('./../src/client/**/build/**/*.*')
-    ],
-    serverFiles: pathHelper.resolve('./../src/**/server/**/*.*'),
-    bootFiles: [
-        pathHelper.resolve('./../src/**/boot/**/*.*'),
-        pathHelper.resolve('./../src/index.js'),
-        pathHelper.resolve('./../package.json')
-    ],
-    clientReleaseDir: pathHelper.resolve('./../dist/client'),
-    releaseDir: pathHelper.resolve('./../dist'),
+	clientFiles: [
+		pathHelper.resolve('./../src/client/index.html'),
+		pathHelper.resolve('./../src/client/loadResource.js'),
+		pathHelper.resolve('./../src/client/favicon.ico'),
+		pathHelper.resolve('./../src/client/bower.json'),
+		pathHelper.resolve('./../src/client/**/build/**/*.*')
+	],
+	serverFiles: pathHelper.resolve('./../src/**/server/**/*.*'),
+	bootFiles: [
+		pathHelper.resolve('./../src/**/boot/**/*.*'),
+		pathHelper.resolve('./../src/index.js'),
+		pathHelper.resolve('./../package.json')
+	],
+	clientReleaseDir: pathHelper.resolve('./../dist/client'),
+	releaseDir: pathHelper.resolve('./../dist'),
 
-    runWebBy: 'cmd', // 'cmd','in' // default is in
-    serverUnitConfig: pathHelper.resolve('./../test/config/server.unit.conf.js'),
-    karmaClientUnitConfig: pathHelper.resolve('./../test/config/karma-client.unit.conf.js'),
-    karmaClientE2eConfig: pathHelper.resolve('./../test/config/karma-client.e2e.conf.js'),
-    protractorClientE2eConfig: pathHelper.resolve('./../test/config/protractor-client.e2e.conf.js'),
+	runWebBy: 'cmd', // 'cmd','in' // default is in
+	serverUnitConfig: pathHelper.resolve('./../test/config/server.unit.conf.js'),
+	karmaClientUnitConfig: pathHelper.resolve('./../test/config/karma-client.unit.conf.js'),
+	karmaClientE2eConfig: pathHelper.resolve('./../test/config/karma-client.e2e.conf.js'),
+	protractorClientE2eConfig: pathHelper.resolve('./../test/config/protractor-client.e2e.conf.js'),
 
-    commonJs: pathHelper.resolve('./../common/**/*.js'),
+	commonJs: pathHelper.resolve('./../common/**/*.js'),
 
-    gulpFile: pathHelper.resolve('./../gulpfile.js'),
-    gulpFiles: pathHelper.resolve('./../gulp/**/*.js'),
+	gulpFile: pathHelper.resolve('./../gulpfile.js'),
+	gulpFiles: pathHelper.resolve('./../gulp/**/*.js'),
 
-    serverJs: pathHelper.resolve('./../src/server/**/*.js'),
-    serverUnitTestJs: pathHelper.resolve('./../test/server/unit/**/*.js'),
+	serverJs: pathHelper.resolve('./../src/server/**/*.js'),
+	serverUnitTestJs: pathHelper.resolve('./../test/server/unit/**/*.js'),
 
-    src: pathHelper.resolve('./../src'),
+	src: pathHelper.resolve('./../src'),
 
-    clientHtml: pathHelper.resolve('./../src/client/**/*.html'),
-    clientCss: pathHelper.resolve('./../src/client/**/*.css'),
+	clientHtml: pathHelper.resolve('./../src/client/**/*.html'),
+	clientCss: pathHelper.resolve('./../src/client/**/*.css'),
 
-    clientJs: pathHelper.resolve('./../src/client/**/*.js'),
-    clientTemplates: pathHelper.resolve('./../src/client/templates/**/*.html'),
+	clientJs: pathHelper.resolve('./../src/client/**/*.js'),
+	clientTemplates: pathHelper.resolve('./../src/client/templates/**/*.html'),
 
-    clientBuildFiles: '!' + pathHelper.resolve('./../src/client/build/*.*'),
+	clientBuildFiles: '!' + pathHelper.resolve('./../src/client/build/*.*'),
 
-    karmaClientUnitTestJs: pathHelper.resolve('./../test/client/unit/**/*.js'),
-    karmaClientBddTestJs: pathHelper.resolve('./../test/client/bdd/karma/**/*.js'),
-    protractorClientBddTestJs: pathHelper.resolve('./../test/client/bdd/protractor/**/*.js')
+	karmaClientUnitTestJs: pathHelper.resolve('./../test/client/unit/**/*.js'),
+	karmaClientBddTestJs: pathHelper.resolve('./../test/client/bdd/karma/**/*.js'),
+	protractorClientBddTestJs: pathHelper.resolve('./../test/client/bdd/protractor/**/*.js')
 };
 
-config.minFiles = [config.releaseDir + '/**/*.js'];
+config.minFiles = [config.clientReleaseDir + '/**/*.js'];
 
 module.exports = config;
--- gulp/tasks/codeQuality/min.js
+++ gulp/tasks/codeQuality/min.js
@@ -2,26 +2,30 @@
 
 
 var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
 
 function min(context, done) {
-    context.gulp
-        .src(context.config.minFiles)
-        .pipe(uglify())
-        .pipe(context.gulp.dest(context.config.releaseDir));
+	context.gulp
+		.src(context.config.minFiles)
+		.pipe(uglify())
+		.pipe(rename(function(path) {
+			path.extname = ".min" + path.extname;
+		}))
+		.pipe(context.gulp.dest(context.config.clientReleaseDir));
 
-    done(null);
+	done(null);
 }
 
 function createTask(context) {
-    var tasks = context.tasks;
+	var tasks = context.tasks;
 
-    context.gulp.task(
-        tasks.min,
-        'js min',
-        function(done) {
-            min(context, done);
-        }
-    );
+	context.gulp.task(
+		tasks.min,
+		'js min',
+		function(done) {
+			min(context, done);
+		}
+	);
 }
 
 module.exports = createTask;
