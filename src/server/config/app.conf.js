49f71b0ffc961c10b2f9c921f36559eb845fb7c5
zhangaz1 <zhangaz1@hotmail.com>
2015-03-24 09:36:51 +0800

    更改localhost为127.0.0.1

--- src/server/config/app.conf.js
+++ src/server/config/app.conf.js
@@ -3,7 +3,7 @@
 var pathHelper = require('path-helper')(__dirname);
 
 module.exports = {
-	mongoUrl: 'mongodb://localhost:27017/ANM',
+	mongoUrl: 'mongodb://127.0.0.1:27017/ANM',
 	port: process.env.PORT || 3000,
 	client: pathHelper.resolve('../../client')
 };
--- test/config/protractor-client.e2e.conf.js
+++ test/config/protractor-client.e2e.conf.js
@@ -1,20 +1,20 @@
 'use strict';
 
 exports.config = {
-    seleniumAddress: 'http://localhost:4444/wd/hub',
+	seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
 
-    // seleniumServerJar: 'E:/TestTools/selenium-server-standalone-2.44.0.jar',
+	// seleniumServerJar: 'E:/TestTools/selenium-server-standalone-2.44.0.jar',
 
-    chromeDriver: 'E:/TestTools/chromedriver.exe',
+	chromeDriver: 'E:/TestTools/chromedriver.exe',
 
-    // Capabilities to be passed to the webdriver instance.
-    capabilities: {
-        'browserName': 'chrome'
-    },
+	// Capabilities to be passed to the webdriver instance.
+	capabilities: {
+		'browserName': 'chrome'
+	},
 
-    // Options to be passed to Jasmine-node.
-    jasmineNodeOpts: {
-        showColors: true,
-        defaultTimeoutInterval: 30000
-    }
+	// Options to be passed to Jasmine-node.
+	jasmineNodeOpts: {
+		showColors: true,
+		defaultTimeoutInterval: 30000
+	}
 };
